8f6f53ffa56e263d06c4a4f646483972365dbdc8
DeniseBarbosa <deniseluvoir@gmail.com>
2023-03-28 16:27:20 -0300

    Utilizando a função Map

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,38 +7,40 @@
 //..continua eu retorno >> (function (Response) {console.log(Response);});
 // arrow function => uma função simplificada ( função especial )
 //convertPokemonToHtml >> vai pegar uma lista de pokemons
+
+/**
+ * Função que converte um objeto pokemon em HTML
+ *
+ * @param {Object} pokemon - objeto com informações do pokemon
+ * @returns {String} - string com o HTML do pokemon
+ */
 function convertPokemonToHtml(pokemon) {
-    return `<li class="pokemon">
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-        <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
-        </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-            alt="${pokemon.name}">
-    </div>
-
-</li>`
+    // Utiliza string template para retornar o HTML do pokemon
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
 }
-//vai add mais uma lista
-const pokemonList = document.getElementById('pokemonList')
 
-    // pokeApi.getPokemons() refere-se ao arquio poke-api.js
-    pokeApi.getPokemons().then((pokemons)=>{
-        const listItens = []
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            //vai add uma lista
-            listItens.push(convertPokemonToHtml(pokemon))
-            
-        }
+// Obtém a referência do elemento com id "pokemonList"
+const pokemonList = document.getElementById('pokemonList');
 
-        console.log(listItens)
-    })
-   
-    //vai ser executado independentemente se a respota com com sucesso ou erro
-    // .finally( () => 
-    //     console.log('Requisição concluida!!')
-    //
+// Chama o método getPokemons da variável pokeApi e atribui o resultado à variável pokemons
+pokeApi.getPokemons().then((pokemons = []) => {
+    // Utiliza o operador += para adicionar à lista de pokemons os pokemons obtidos da API
+    // Utiliza o método map para aplicar a função convertPokemonToHtml a cada pokemon e gerar uma lista de strings HTML
+    // Utiliza o método join para concatenar todas as strings HTML em uma só
+    // O resultado é adicionado ao HTML da lista de pokemons
+    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('');
+});
+
